e9d2dd039a92b658d657419b1b52ecb85319fcf6
WorshipMeOrElse <sylviaviajung@gmail.com>
2023-04-19 17:59:57 -0700

    basic ping command on poise

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -1,11 +1,8 @@
-use serenity::builder::CreateApplicationCommand;
-use serenity::model::prelude::interaction::application_command::CommandDataOption;
-use serenity::builder::CreateInteractionResponseData;
+use crate::Error;
 
-pub fn run(_options: &[CommandDataOption]) -> CreateInteractionResponseData {
-    return CreateInteractionResponseData::default().content("VIOLENCE???!").to_owned()
-}
-
-pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
-    command.name("ping").description("A ping command")
+#[poise::command(slash_command)]
+pub async fn ping(ctx: poise::Context<'_, (), Error>) -> Result<(), Error> {
+    let response = "pong!";
+    ctx.say(response).await?;
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,35 @@
-mod commands;
-
-use std::env;
-
-use serenity::async_trait;
-use serenity::builder::CreateInteractionResponseData;
-use serenity::model::application::command::Command;
-use serenity::model::application::interaction::{Interaction, InteractionResponseType};
-use serenity::model::channel::Message;
-use serenity::model::gateway::Ready;
-use serenity::prelude::*;
-
 use dotenv::dotenv;
+use poise::serenity_prelude as serenity;
 
-struct Handler;
+struct UserData {}
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, UserData, Error>;
 
-#[async_trait]
-impl EventHandler for Handler {
-    async fn interaction_create(&self, context: Context, interaction: Interaction) {
-        if let Interaction::ApplicationCommand(command) = interaction {
-            println!("DEBUG: cmd received: {:#?}", command);
-
-            let content = match command.data.name.as_str() {
-                "ping" => commands::ping::run(&command.data.options),
-                "embed" => commands::embed::run(&command.data.options),
-                _ => CreateInteractionResponseData::default().content("not implemented :(").to_owned()
-            };
-
-            if let Err(why) = command
-                .create_interaction_response(&context.http, |response| {
-                    response
-                        .kind(InteractionResponseType::ChannelMessageWithSource)
-                        .interaction_response_data(|r| {*r = content; r})
-                })
-                .await
-            {
-                println!("Cannot respose to slash command: {}", why)
-            }
-        }
-    }
-
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "no.").await {
-                println!("Error sending message: {:?}", why);
-            }
-        }
-    }
-
-    async fn ready(&self, context: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
-
-        let _guild_commands =
-            Command::create_global_application_command(&context.http, |command| {
-                commands::ping::register(command);
-                commands::embed::register(command)
-            })
-            .await;
-    }
+#[poise::command(slash_command)]
+async fn ping(ctx: Context<'_>) -> Result<(), Error> {
+    let response = "pong!";
+    ctx.say(response).await?;
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
-    let token = env::var("TOKEN").expect("Expected a token in the environment");
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![ping()],
+            ..Default::default()
+        })
+        .token(std::env::var("TOKEN").expect("missing bot token"))
+        .intents(serenity::GatewayIntents::non_privileged())
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(UserData {})
+            })
+        });
 
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
-
-    let mut client = Client::builder(&token, intents)
-        .event_handler(Handler)
-        .await
-        .expect("Err creating client");
-
-    if let Err(why) = client.start().await {
-        println!("Client error: {:?}", why);
-    }
+    framework.run().await.unwrap();
+    println!("its time to get tangy!");
 }
-
